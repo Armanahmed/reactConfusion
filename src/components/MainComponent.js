82b115d43a946672b6a3cf8ad80367f4f0223f40
Armanahmed <aaarmanapu@gmail.com>
2018-11-13 10:19:39 +0600

    Second Assignment

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -36,6 +37,12 @@ class Main extends Component {
       );
     }
 
+    const AboutPage = () => {
+      return(
+        <About leaders={this.state.leaders} />
+      );
+    }
+
     const DishWithId = ({match}) => {
       return(
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
@@ -51,6 +58,7 @@ class Main extends Component {
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />
           <Route path="/menu/:dishId" component={DishWithId} />
+          <Route path="/aboutus" component={AboutPage} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
