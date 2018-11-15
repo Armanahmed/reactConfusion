8550b31be955e792d9d2a376c320044f4ad77541
Armanahmed <aaarmanapu@gmail.com>
2018-11-15 09:37:51 +0600

    Uncontrolled Form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -29,7 +29,7 @@ class Contact extends Component {
 
     handleInputChange(event) {
       const target = event.target;
-      const value= target.type === 'checkbox' ? target.checked : target.value;
+      const value = target.type === 'checkbox' ? target.checked : target.value;
       const name = target.name;
 
       this.setState({
